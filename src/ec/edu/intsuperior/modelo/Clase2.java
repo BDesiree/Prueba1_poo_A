f370b326541b01c36d10d73eff98c94fdfd0e62b
User <User@DESKTOP-0A5SAM6>
2022-07-07 20:27:39 -0500

    Esta es la prueba de la base de la POO

---
+++ src/ec/edu/intsuperior/modelo/Clase2.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author User
+ */
+public class Clase2 {
+  public static void metodo1() {
+        System.out.println("metodo 1 de clase");
+    }
+
+    public static void metodo2() {
+        System.out.println("metodo 2 de clase");
+    }
+
+    public static void metodo3() {
+        System.out.println("metodo 3 de clase");
+    }
+
+    public static void metodo4() {
+        System.out.println("metodo 4 de clase");
+    }
+
+    public static void metodo5() {
+        System.out.println("metodo 5 de clase");
+    }
+
+    public void metodoInstancia_1() {
+        System.out.println("metodo de instancia 1");
+    }
+
+    public void metodoInstancia_2() {
+        System.out.println("metodo de instancia 2");
+    }
+
+    public void metodoInstancia_3() {
+        System.out.println("metodo de instancia 3");
+    }
+
+    public void metodoInstancia_4() {
+        System.out.println("metodo de instancia 4");
+    }
+
+    public void metodoInstancia_5() {
+        System.out.println("metodo de instancia 5");
+    }
+} 
---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,18 @@
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Clase1;
+import ec.edu.intsuperior.modelo.Clase2;
+import ec.edu.intsuperior.modelo.Clase3;
+
+/**
+ *
+ * @author User
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        Clase1 obj = new Clase1();
+        obj.metodoInstancia_1();
+    }
+
+}
